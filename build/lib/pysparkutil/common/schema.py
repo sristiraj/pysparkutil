c6ddd099350eddb514c5f3e850b89c162cfd38d1
rajsr1 <sristi.raj@novartis.com>
2021-11-18 23:47:05 +0530

    Updated

--- build/lib/pysparkutil/common/schema.py
+++ build/lib/pysparkutil/common/schema.py
@@ -9,6 +9,7 @@ from pyspark.sql.functions import *
 import pyspark
 import os
 from pyspark.sql import SparkSession
+import io
 
 
 class InvalidSchemaDefinition(Exception):
@@ -28,12 +29,12 @@ class Schema(object):
     def set_location(self, path):
         self.location = path
         
-    def _schema_uri_type(self):
-        if self.location.startswith("file://"):
+    def _schema_uri_type(self, path):
+        if path.startswith("file://"):
             location_type = "local"
-        elif self.location.startswith("s3://"):
+        elif path.startswith("s3://"):
             location_type = "s3"
-        elif self.location.startswith("dbfs:/"):
+        elif path.startswith("dbfs:/"):
             location_type = "dbfs"
         else:
             raise InvalidSchemaLocation()
@@ -47,20 +48,32 @@ class Schema(object):
         return self.schema_type
       
     def _schema_data(self):
-        location_type = self._schema_uri_type()
+        location_type = self._schema_uri_type(self.location)
         if location_type == "local":
-            with open(self.location, "rb") as f:
-                data = f.read()
+            try:
+                with open(self.location.replace("file://",""), "rb") as f:
+                    data = f.read()
+            except Exception as e:
+                print("Error reading schema file from {}: ".format(self.location)+str(e))
+                raise e
         elif location_type == "s3":
-            s3 = boto3.client("s3")
-            s3_bucket_index = self.location.replace("s3://","").find("/")
-            s3_bucket = self.location[5:s3_bucket_index+5]
-            s3_key = self.location[s3_bucket_index+6:]
-            obj = s3.get_object(Bucket=s3_bucket, Key=s3_key)            
-            data = obj["Body"].read()
+            try:
+                s3 = boto3.client("s3")
+                s3_bucket_index = self.location.replace("s3://","").find("/")
+                s3_bucket = self.location[5:s3_bucket_index+5]
+                s3_key = self.location[s3_bucket_index+6:]
+                obj = s3.get_object(Bucket=s3_bucket, Key=s3_key)            
+                data = obj["Body"].read().decode("utf-8")
+            except Exception as e:
+                print("Error reading schema file from s3 {}: ".format(self.location)+str(e))    
+                raise e
         elif location_type == "dbfs":
-            with open(self.location.replace("dbfs:","/dbfs")) as f:
-                data = f.read()
+            try:
+                with open(self.location.replace("dbfs:","/dbfs")) as f:
+                    data = f.read()
+            except Exception as e:
+                print("Error reading schema file from {}: ".format(self.location)+str(e))
+                raise e
         else:
           data = None
         return data    
@@ -139,4 +152,48 @@ class Schema(object):
             fieldList.append(structFields)
         schema = StructType(fieldList)
         return schema    
+      
+    def _tmp_schema(self, data):
+        cwd = os.getcwd()
+        sep = os.sep
+        tmp_file = "tmp.avro"
+        tmppath = self.location[:self.location.rfind("/")]+"_tmp"+self.location[self.location.rfind("/"):]
+        pathtype = self._schema_uri_type(tmppath)
+        return_path = ""
+        if pathtype=="local":
+            with open(tmppath.replace("file://",""), "wb+") as fw:
+                writer = DataFileWriter(fw, DatumWriter(), data)
+                writer.close()
+            return_path = tmppath.replace("file://","")   
+        elif pathtype=="s3": 
+            s3 = boto3.client("s3")
+            s3_bucket_index = tmppath.replace("s3://","").find("/")
+            s3_bucket = tmppath[5:s3_bucket_index+5]
+            s3_key = tmppath[s3_bucket_index+6:]
+            print(f"s3 bucket for tmp path {s3_bucket}")
+            print(f"s3 key for tmp path {s3_key}")
+            with open(cwd+sep+tmp_file, "wb+") as fw:
+                writer = DataFileWriter(fw, DatumWriter(), data)
+                writer.close()
+            s3.upload_file(cwd+sep+tmp_file, s3_bucket, s3_key.replace(".avsc",".avro"))
+            return_path = "s3://"+s3_bucket+"/"+s3_key.replace(".avsc",".avro")
+        elif pathtype=="dbfs": 
+            with open(tmppath.replace("dbfs:/","/dbfs/"), "wb+") as fw:
+                writer = DataFileWriter(fw, DatumWriter(), data)
+                writer.close()
+            return_path = tmppath.replace("dbfs:/","/dbfs/")
+        return return_path        
+            
+    def get_unstructured_schema(self):
+        json_data=self.parse_schema()
+        spark = SparkSession.builder.appName("schema").getOrCreate()
+        sc = spark.sparkContext
+        tmppath = self._tmp_schema(json_data)  
+        df = spark.read.format('avro').load(tmppath)  
+        return df.schema  
 
+if __name__=="__main__":
+  sch = Schema()
+  sch.set_location("dbfs:/FileStore/tables/sample_json-2.avsc")
+  print(sch.get_unstructured_schema())
+  
